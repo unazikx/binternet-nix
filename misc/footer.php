1ce59287957a13d285301cdf72a6d4013bea5423
unazikx <xfalwa@gmail.com>
2026-06-19 14:44:09 +0900

    change urls

--- misc/footer.php
+++ misc/footer.php
@@ -1,6 +1,6 @@
 <footer>
-    <a href="https://github.com/Ahwxorg/binternet/" target="_blank">Source code</a>
-    <a href="https://github.com/Ahwxorg/binternet#legal-notice" target="_blank">Legal notice</a>
+    <a href="https://github.com/unazikx/binternet-nix/" target="_blank">Source code</a>
+    <a href="https://github.com/unazikx/binternet-nix#legal-notice" target="_blank">Legal notice</a>
 
 <?php
     if (isset($images)) {
